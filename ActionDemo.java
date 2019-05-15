818575f3a984dfb0d4db2a9405314bbbecc20676
bharat <47022219+Hiremath19@users.noreply.github.com>
2019-05-15 16:52:23 +0530

    Add files via upload

---
+++ ActionDemo.java
@@ -0,0 +1,43 @@
+package com.sankir;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.PairFunction;
+import scala.Tuple2;
+
+import java.util.Arrays;
+import java.util.Map;
+
+public class ActionDemo {
+    public static void main(String[] args) {
+        SparkConf sparkConf = new SparkConf().setMaster("local").setAppName("Sankir");
+
+        JavaSparkContext sparkContext = new JavaSparkContext(sparkConf);
+
+        JavaRDD<String> RDD3 = sparkContext.parallelize(Arrays.asList("aaa,30", "bbb,100", "aaa,20",
+                "ccc,30", "eee,50", "ddd,40", "bbb,60", "ccc,80", "eee,100"));
+
+        JavaPairRDD<String, Integer> RDD4 = RDD3.mapToPair(new PairFunction<String, String, Integer>() {
+            @Override
+            public Tuple2<String, Integer> call(String s) throws Exception {
+                return new Tuple2(s.split(",")[0], Integer.parseInt(s.split(",")[1]));
+            }
+        });
+        System.out.println(RDD4.collect());
+
+        System.out.println("Count by key:");
+        System.out.println(RDD4.countByKey());
+
+        System.out.println("Collect as Map");
+        Map<String,Integer> m1 = RDD4.collectAsMap();
+
+        System.out.println(m1);
+
+        System.out.println(RDD4.lookup("bbb"));
+
+
+
+    }
+}
